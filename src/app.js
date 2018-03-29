33a0a20f38892e30c50c048d6d3ad80f6e2e1d66
AKomarov <Andrey.Komarov@firstlinesoftware.com>
2018-03-29 17:48:06 +0300

    Proj v01

---
+++ src/app.js
@@ -0,0 +1,25 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import app from './app.vue'
+import { sync } from 'vuex-router-sync'
+import { routes } from './system/routes'
+import store from './store/index'
+import VueNotifications from 'vue-notifications'
+import options from './system/vueNotifications'
+
+Vue.use(VueRouter)
+Vue.use(VueNotifications, options)
+
+export const router = new VueRouter({
+  mode: 'history',
+  routes
+})
+
+sync(store, router)
+
+new Vue({
+  el: '#app',
+  store,
+  router,
+  render: h => h(app)
+})
